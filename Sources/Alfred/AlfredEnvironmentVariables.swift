d582ce700bd4a0b24ed1521565b1bc24c8f90292
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2021-05-26 08:50:49 +0530

    Add class to access Alfred specific env vars

---
+++ Sources/Alfred/AlfredEnvironmentVariables.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+extension Alfred {
+  /// Values of Alfred-specific environment variables
+  /// passed to the program when the program was started.
+  /// Such values are passed only if the program was started
+  /// from within a workflow.
+  ///
+  /// Note: If this is going to be a long-running program like
+  /// a daemon or a http server, that services multiple workflows,
+  /// it is important to remember that these values reflect
+  /// the environment **in which the program was launched**.
+  static let envVarsAtInvocation: AlfredEnvironmentVariables? = {
+    let env = ProcessInfo.processInfo.environment
+
+    // alfred_preferences is an env var that's always set
+    // by Alfred for processes that are started in workflows
+    if env["alfred_preferences"] != nil {
+      return AlfredEnvironmentVariables(env: env)
+    }
+
+    return nil
+  }()
+}
+
+/**
+ [Reference documentation][1]
+ [1]: https://www.alfredapp.com/help/workflows/script-environment-variables/
+ */
+class AlfredEnvironmentVariables {
+  private let env: [String: String]!
+
+  fileprivate init(env: [String: String]) {
+    self.env = env
+  }
+
+  lazy var prefsDir: URL = URL(fileURLWithPath: env["alfred_preferences"]!)
+  lazy var themeID: String = env["alfred_theme"]!
+  lazy var workflowID: String = env["alfred_workflow_bundleid"]!
+  lazy var debug: Bool = env["alfred_debug"]! == "1"
+}
